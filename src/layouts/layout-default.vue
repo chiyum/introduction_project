0ec882e1cc53429f5883f4e7865034a4b974915b
chiyum <pray870712@gmail.com>
2023-05-25 16:10:22 +0800

    feat: 專案架構push

---
+++ src/layouts/layout-default.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="pb-32">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { useI18n } from "@/hooks/use-i18n";
+
+export default {
+  setup() {
+    const { t } = useI18n();
+    // layout也可以做邏輯處理
+    return {
+      t,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+// @import "@/assets/scss/layout.scss";
+.layout-show {
+  visibility: visible;
+}
+.layout-hide {
+  visibility: hidden;
+}
+.notify-tip {
+  &::before {
+    right: 0.4rem;
+    top: 3px;
+  }
+  &.nav-icon {
+    &::before {
+      right: -4px;
+      top: 2px;
+    }
+  }
+}
+</style>

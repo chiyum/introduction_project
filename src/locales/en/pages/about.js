6d8557dc58856902d203f7533d914aa10ec0e6b6
chiyum <pray870712@gmail.com>
2024-07-15 10:41:00 +0800

    feat: 🎸 自我介紹英文沒顯示bug修正

--- src/locales/en/pages/about.js
+++ src/locales/en/pages/about.js
@@ -2,33 +2,47 @@ export default {
   title: "About",
   "introduce.title": "I am Huang Sheng-Hong,",
   "introduce.title.2": "a Front-End Engineer.",
-  "introduce.caption": `I am a Front-End Engineer with nearly 3 years of hands-on development experience, passionately devoted to web design and development.
+  "introduce.caption": `I am a front-end engineer with nearly 3 years of practical development experience, passionately dedicated to web design and development.
   I am proficient in HTML, CSS, JavaScript, TypeScript, Vue, Vuex, Nuxt.js, Node.js, express, and React.
-  Besides essential version control with Git, I understand and utilize technologies like Docker, Golang, MongoDB, and MySQL.
-  I enjoy exploring the principles behind functionalities, having crafted plugins like Swiper and tree structures, and I like studying architecture and optimization.
-  I am adept at solving problems independently and can effectively collaborate with other developers, designers, and project managers. I am also willing to work overtime to meet company schedules.`,
+  Beyond essential version control with Git, I also understand and utilize technologies like Docker, Golang, mongoDB, and MYSQL.
+  I enjoy exploring the principles behind functionalities, have handcrafted plugins like Swiper and tree structures, and like to delve into software architecture and optimization.
+  I am adept at solving problems independently and can effectively communicate and collaborate with other developers, designers, and PMs. I am also willing to work overtime to align with company schedules.`,
   "introduce.label.birthday": "Birthday:",
   "introduce.label.age": "Age:",
   "introduce.label.web": "Github:",
   "introduce.label.mail": "Email:",
   "introduce.label.phone": "Contact Number:",
   "introduce.label.city": "City:",
-  "introduce.label.city.answer": "Taichung, Taiwan",
+  "introduce.label.city.answer": "Taichung City, Taiwan",
   "experience.experience": "Experience",
   "experience.education": "Education",
-  "experience.experience.title.1": "Mega Digital Ltd.",
-  "experience.experience.caption.1": `Throughout my tenure at the company, I have served as the lead front-end developer on numerous projects. I have been deeply involved in every project, paying close attention to each detail, from discussing user interface design with PMs and designers to optimizing the code.
-Moreover, I had the opportunity to work alongside the company's senior front-end engineers, exploring and improving the architecture of our projects. This experience taught me numerous new techniques and best practices, and showed me how teamwork can drive project success and foster personal growth.`,
-  "experience.experience.title.2": "Qunyi Digital Ltd.",
-  "experience.experience.caption.2": `Joining this company marked the beginning of my venture into the software development industry. Here, I first experienced the actual employment model and deepened my understanding of job requirements and expectations.
-I had the opportunity to learn and experiment with various development frameworks, trying different tools and learning how to select the best framework for specific projects. These experiences not only improved my technical skills but also gave me a comprehensive understanding of software development. Working at this company enhanced my technical abilities and helped me understand the practical operations of software development, reinforcing the importance of continuous learning and experimentation.`,
+  "experience.experience.title.1": "Mega Digital Co., Ltd.",
+  "experience.experience.caption.1": `Throughout my tenure at the company, I have served as the front-end lead for many projects. I have a deep understanding of the projects I am involved in, paying close attention to every detail, from discussing user interface design with PMs and designers to optimizing the code.
+Moreover, I have had the opportunity to work alongside senior front-end engineers in the company, researching and improving the project's architecture. This experience has taught me numerous new technologies and best practices, and also shown me how teamwork can enhance the success of a project and foster personal growth.`,
+  "experience.experience.title.2": "Qunyi Digital Co., Ltd.",
+  "experience.experience.caption.2": `Joining this company marked my entry into the software development industry. Here, I first experienced the practical employment model and gained a deeper understanding of job requirements and expectations.
+In the company, I had the opportunity to learn and experiment with various development frameworks. I experimented with different tools and learned how to select the best frameworks for specific projects. These experiences enhanced my technical skills and gave me a comprehensive understanding of software development. Working in this company not only improved my technical abilities but also gave me a practical insight into how software development operates, underscoring the importance of continuous learning and experimentation.`,
   "experience.experience.title.3": "Other Industries",
-  "experience.experience.caption.3": `Before entering the software industry, I explored various industries and workplaces. I have served as an assistant manager in the chain restaurant industry, managing personnel and inventory; I have also worked as a reserve officer and front desk clerk in the hotel industry, as well as a construction apprentice and teppanyaki chef. Each experience has taught me valuable skills.
-These extensive practical experiences not only enhanced my professional skills but also taught me how to manage crises, get along with colleagues, and effectively respond to customers. These experiences have laid a solid foundation for my new path in the software industry.`,
-  "experience.experience.title.4": "Xiang Yi Digital Ltd.",
-  "experience.experience.caption.4": `During my time at the company as a front-end engineer, I took on a special challenge: developing a game and its architecture using the Vue framework. Typically, frameworks like Vue are seldom used for game development, which made this task not only a technical adventure but also a learning and growth opportunity. In this project, I was involved in front-end interface design and delved deep into back-end data interaction and maintenance. This experience was crucial in enhancing my front-end skills and understanding of modern web applications' architecture.
-Moreover, I used React and Vite to build the game's backend management system. This system supported game data management and analysis, improving the efficiency and flexibility of game operations. This project not only honed my front-end development skills but also deepened my understanding of modern web development technologies and tools.`,
-  "experience.experience.title.5": "Jacy Digital Ltd.",
-  "experience.experience.caption.5": `I am a front-end engineer, but at my current company, I have started transitioning to a full-stack engineer role. This journey has allowed me to delve into back-end development, personally designing and writing APIs that not only enhance my understanding of the entire application architecture but are also actively used in the company's projects, effectively improving project efficiency and flexibility.
-In addition, I have had the opportunity to integrate APIs from third-party services, such as Google Sheets, allowing me to handle more complex data interactions and optimize our data flow. These experiences have not`,
+  "experience.experience.caption.3": `Before entering the software industry, I explored various workplaces across multiple industries. I have served as an assistant manager in the chain restaurant industry, handling personnel and inventory; I have also worked in the hospitality industry as a management trainee and front desk clerk, and even as a construction apprentice and teppanyaki chef. Each experience has provided me with valuable knowledge and skills.
+In these diverse practical experiences, I not only honed my professional skills but also learned how to handle crises, maintain harmonious relationships with colleagues, and effectively manage customer interactions. These experiences have laid a solid foundation for my path in the software industry.`,
+  "experience.experience.title.4": "Yoyi Digital Co., Ltd.",
+  "experience.experience.caption.4": `During my time as a front-end engineer at the company, I took on a special challenge: developing games and their related architectures using the Vue framework. Typically, frameworks like Vue are rarely used for game development, making this task not only a technical adventure but also a learning and growth opportunity for me. In this project, I was involved in both the front-end interface design and the backend data interaction and maintenance. This experience was crucial for enhancing my front-end skills and understanding the architecture of modern web applications.
+Additionally, I used React and Vite to build the game's backend management system. This system supports the management and analysis of game data, enhancing the operational efficiency and flexibility of the game. In this project, I not only exercised my front-end development skills but also deepened my understanding of modern web development technologies and tools.`,
+  "experience.experience.title.5": "Jayce Digital Co., Ltd.",
+  "experience.experience.caption.5": `I am a front-end engineer, but in my current company, I have started transitioning towards a full-stack engineer role. This journey has allowed me to delve deeper into backend development, beginning to design and write APIs myself, which not only enhances my understanding of the entire application architecture but also has been actively used in the company's projects, effectively improving project efficiency and flexibility.
+In addition, I have had the opportunity to integrate with third-party services through API connections, such as Google Sheets, allowing me to handle more complex data interactions and optimize our data flows. These experiences have not only enriched my tech stack but also boosted my confidence in solving practical problems and meeting user needs.
+These transitions have enabled me to more comprehensively understand and participate in various stages of projects, from front-end user interface design to backend data management, allowing me to provide professional technical support and solutions, making me more versatile and adaptable in the tech field.`,
+  "experience.education.title.1": "High School Equivalent",
+  "experience.education.caption.1": `When I was younger, due to naivety, I did not fully grasp the importance of education. However, as I grew older and broadened my horizons, I recognized the value of learning and decided to make up for the education I had neglected in the past.
+To achieve this goal, I resolved to continue my education and compensate for the qualifications I had missed. In this process, I not only learned academic knowledge but also deeply experienced the joy of learning and gained a clearer plan for my future career. I now understand more than ever that the process of learning and continuous improvement is invaluable for both personal and professional growth.
+Therefore, although I neglected my studies in my earlier years, I learned from my mistakes and laid a firmer foundation for my future.`,
+  "experience.education.title.2": "Taichung Vocational High School",
+  "experience.education.caption.2": `In my youth, I was fortunate to be admitted to the school of my choice. However, because I did not fully understand the importance of education at the time, I chose to take a break from school in the second year of high school to enter the workforce.
+At that time, filled with curiosity and anticipation for the unknown world, I was eager to explore the diverse aspects of life. I believed that through actual work experience, I could better understand myself and discover my true interests and direction.
+Although I entered society early, I never forgot the importance of learning. I continuously sought knowledge and improved myself, which allowed me to better adapt and grow in the workplace.
+Looking back, I do not regret my decision, because that experience shaped who I am today. I believe that whether in school or the workplace, as long as I maintain my love and curiosity for knowledge, I can find my own path of learning and growth in life.`,
+  "experience.education.title.3": "Puli Junior High School",
+  "experience.education.caption.3": `During my middle school years, I developed an inseparable bond with computers. Because of my frequent gaming, I had a unique interest in computer operations, giving me a solid foundation and proficiency in this area.
+The education at my school also provided me with ample opportunities to deepen my computer skills. Through the school's resources, I participated in related competitions, further strengthening my understanding and operational skills in computing. Additionally, I worked hard to obtain related certifications, further proving my professional ability in this field.
+Therefore, this period also served as a stepping stone for me entering the software industry.`,
 };
